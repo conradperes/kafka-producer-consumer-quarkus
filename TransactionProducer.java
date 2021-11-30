fc66c5af5852243e170b7a51ecd9d7b3f8c8b466
CONRAD PERES <conrad.marques.peres@gmail.com>
2021-11-30 15:22:31 -0300

    Add files via upload

--- TransactionProducer.java
+++ TransactionProducer.java
@@ -12,6 +12,8 @@ import java.util.UUID;
 
 @ApplicationScoped
 public class TransactionProducer {
+    public static final String STEP_STATUS_STARTED = "STARTED";
+    public static final String CURRENT_STEP_VALIDATION = "VALIDATION_PAYMENT";
     private final Logger logger = Logger.getLogger(TransactionProducer.class);
 
 
@@ -23,8 +25,10 @@ public class TransactionProducer {
     public void sendTransactionToKafka(Transaction transaction) {
         UUID uuid = UUID.randomUUID();
         transaction.id = uuid.toString();
-        transaction.payload = Long.toString(new Date().getTime());
-        logger.info("Producer of transacion="+transaction);
+        //transaction.payload = Long.toString(new Date().getTime());
+        transaction.stepstatus = STEP_STATUS_STARTED;
+        transaction.currentstep = CURRENT_STEP_VALIDATION;
+        logger.info("Producer of transacion=\t"+transaction);
         emitter.send(Record.of(transaction.id.hashCode(), transaction));
     }
 }
